ba5275e0c771deb9d05db3b554ed0ce56545b5ca
Carlos Eduardo Mattos <carloseduardodesouzamattos@gmail.com>
2026-07-14 16:10:32 -0300

    Cria versão inicial da função moverDisco

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-let discoSelecionado = null; /*guarda disco selecionado*/
+let discoSelecionado = null; //guarda disco selecionado
+let torreSelecionada = null; //guarda torre selecionada
 
 function selecionaDisco(disco) {
   /*função para selecionar disco*/
-  if (discoSelecionado === null) {
-    /*seleciona disco caso nada esteja selecionado*/
+  if (discoSelecionado === null) {//seleciona disco caso nada esteja selecionado
     discoSelecionado = disco;
     discoSelecionado.classList.add("selecionado");
-    console.log(`${disco.id} selecionado`); /*confirmação visual*/
+    console.log(`${disco.id} selecionado`); //confirmação visual
   } else {
-    if (discoSelecionado === disco) {
-      /* deseleciona disco caso o mesmo ja esteja selecionado*/
+    if (discoSelecionado === disco) {//deseleciona disco caso o mesmo ja esteja selecionado
       discoSelecionado.classList.remove("selecionado");
       discoSelecionado = null;
-      console.log(`${disco.id} deselecionado`); /*confirmação visual*/
+      console.log(`${disco.id} deselecionado`); //confirmação visual
 
     } else{
-        console.log(`Disco anterior ${discoSelecionado.id} / Novo disco ${disco.id}`);/*confirmação visual*/
-        discoSelecionado.classList.remove("selecionado"); /* deseleciona disco anterior*/
-        disco.classList.add("selecionado"); /*seleciona disco clickado*/
+        console.log(`Disco anterior ${discoSelecionado.id} / Novo disco ${disco.id}`);//confirmação visual
+        discoSelecionado.classList.remove("selecionado"); //deseleciona disco anterior*
+        disco.classList.add("selecionado"); //seleciona disco clickado
         discoSelecionado = disco;
     }
   }
 }
 
-let discos =document.querySelectorAll('.disco'); /*cria uma variavel que guarda todos os discos*/
+let discos =document.querySelectorAll('.disco'); //cria uma variavel que guarda todos os discos
 
-discos.forEach(disco => {/* adiciona evento para todos os discos*/
-    disco.addEventListener('click',function(){selecionaDisco(this);});/*chama a função selecionaDisco passando disco como parametro*/
+discos.forEach(disco => {//adiciona evento para todos os discos
+    disco.addEventListener('click',function(event){
+        event.stopPropagation();// corrige bug de selecionar disco e torre ao mesmo tempo
+        selecionaDisco(this);//chama a função selecionaDisco passando disco como parametro
+    });
     
 });
+
+
+/*TORRES*/
+
+let torres = document.querySelectorAll('.torre'); // Pega todas as torres
+
+torres.forEach(torre => {
+    torre.addEventListener('click', function() {moverDisco(this);});//chama a função moverDisco passando torre como parametro
+});
+
+function moverDisco (torre){
+    if(discoSelecionado===null){//caso nenhum disco esteja selecionado
+        console.log(`Nenhum disco selecionado`);
+        return;
+    }
+    torre.appendChild(discoSelecionado);//move disco selecionado para torre selecionada
+        console.log(`disco ${discoSelecionado.id} movido`)
+    discoSelecionado.classList.remove('selecionado');
+    discoSelecionado=null;
+}
